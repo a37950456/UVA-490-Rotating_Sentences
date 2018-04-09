fa0c5b33fa50c84eb016dcab285ef5856f27b8cc
ChiaTien <a37950456@yahoo.com.tw>
2018-04-09 15:31:46 +0800

    Initial Commit

---
+++ UVA-490-Rotating_Sentences/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UVA-490-Rotating_Sentences
+//
+//  Created by Tina Tsai on 2018/4/9.
+//  Copyright © 2018年 Tina Tsai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
